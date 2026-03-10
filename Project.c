f9d2ed0c385b531c95e7512f5d592f1c23c48e7f
RV <rv893302@gmail.com>
2026-03-10 23:37:42 +0530

    Add function to print line in time delay

--- Project.c
+++ Project.c
@@ -10,8 +10,33 @@ void sleep_ms(int ms) { Sleep(ms); }
 void sleep_ms(int ms) { usleep(ms * 1000); }
 #endif
 
+void p_line(const char *line, float seconds);
+
 int main()
 {
 
 }
 
+// FUNCTION PROTOTYPE
+
+// PRINT LINE WITH DELAY
+void p_line(const char *line, float seconds)
+{
+    int len = strlen(line);
+    if (len == 0 || seconds <= 0) // BASE CONDITION FOR NO LINE
+        return;
+
+    int total_ms = (int)(seconds * 1000); // SETTING TIME IN MICROSECONDS
+    int per_char_delay = total_ms / len;
+
+    // PRINTING LINE
+    if (per_char_delay < 1)
+        per_char_delay = 1;
+
+    for (int i = 0; line[i] != '\0'; i++)
+    {
+        putchar(line[i]);
+        fflush(stdout);
+        sleep_ms(per_char_delay);
+    }
+}
